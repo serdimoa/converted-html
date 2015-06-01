0cb5cfd1256a12c2482f7020e7feb6886d7f2427
Dima <serdimoa@gmail.com>
2015-06-01 22:14:13 +0300

    last change

---
+++ public/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    
+    var jsondb = $.getJSON("/public/database/data.json",function(data){
+    $('#my-ajax-table').dynatable({
+        dataset: {
+        records: data
+        }
+    });
+    });
+    
+    
+   $(".menu").affix({
+        offset: { 
+            top: 80 
+     	}
+    });
+    
+    $.dynatableSetup({
+    features: {
+    paginate: true,
+    pushState: true,
+    search: true,
+    recordCount: false,
+    perPageSelect: false
+    },
+         inputs: {
+             paginationPrev: "Предыдущая",
+    paginationNext: 'Следующая',
+         }
+    });
+    
+
+
+});
+
+ 
